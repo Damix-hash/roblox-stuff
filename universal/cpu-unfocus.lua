2ece82b65d47e46808c04d9032b08677f1722066
Damix <158813145+Damix-hash@users.noreply.github.com>
2026-02-21 21:25:57 +0100

    Update cpu-unfocus.lua

--- universal/cpu-unfocus.lua
+++ universal/cpu-unfocus.lua
@@ -1,3 +1,5 @@
+repeat task.wait() until game:IsLoaded()
+
 local RunService = game:GetService("RunService")
 local CoreGui = game:GetService("CoreGui")
 local UserInputService = game:GetService("UserInputService")
