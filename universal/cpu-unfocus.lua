19a914b79eb123483040afb9884c42f98497181b
Damix <158813145+Damix-hash@users.noreply.github.com>
2026-02-22 13:57:45 +0100

    Update cpu-unfocus.lua

--- universal/cpu-unfocus.lua
+++ universal/cpu-unfocus.lua
@@ -4,20 +4,6 @@ local RunService = game:GetService("RunService")
 local CoreGui = game:GetService("CoreGui")
 local UserInputService = game:GetService("UserInputService")
 
-local RobloxGui = CoreGui and CoreGui:FindFirstChild("RobloxGui")
-local SettingsClippingShield = RobloxGui and RobloxGui:FindFirstChild("SettingsClippingShield")
-local SettingsShield = SettingsClippingShield and SettingsClippingShield:FindFirstChild("SettingsShield")
-local MenuContainer = SettingsShield and SettingsShield:FindFirstChild("MenuContainer")
-local Page = MenuContainer and MenuContainer:FindFirstChild("Page")
-local PageViewClipper = Page and Page:FindFirstChild("PageViewClipper")
-local PageView = PageViewClipper and PageViewClipper:FindFirstChild("PageView")
-local PageViewInnerFrame = PageView and PageView:FindFirstChild("PageViewInnerFrame")
-local InnerPage = PageViewInnerFrame and PageViewInnerFrame:FindFirstChild("Page")
-local MaxFrameRateFrame = InnerPage and InnerPage:FindFirstChild("Maximum Frame RateFrame")
-local DropDownFrameButton = MaxFrameRateFrame and MaxFrameRateFrame:FindFirstChild("DropDownFrameButton")
-local DropDownFrameTextLabel = DropDownFrameButton and DropDownFrameButton:FindFirstChild("DropDownFrameTextLabel")
-
--- i prefered to use gui to path script and write all of it myself instead of digging for another half an hour for properget local client get max framerate
 local existingGui = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("UnfocusBlack")
 if existingGui then existingGui:Destroy() end
 
@@ -36,7 +22,27 @@ blackFrame.ZIndex = math.huge
 blackFrame.Visible = false
 blackFrame.Parent = blackGui
 
--- black gui because white is an eye sore even though set3drenderingenabled is already doing the job
+local userFpsCap = 60
+
+local function getMaxFrameRate()
+    local RobloxGui = CoreGui:FindFirstChild("RobloxGui")
+    local SettingsClippingShield = RobloxGui and RobloxGui:FindFirstChild("SettingsClippingShield")
+    local SettingsShield = SettingsClippingShield and SettingsClippingShield:FindFirstChild("SettingsShield")
+    local MenuContainer = SettingsShield and SettingsShield:FindFirstChild("MenuContainer")
+    local Page = MenuContainer and MenuContainer:FindFirstChild("Page")
+    local PageViewClipper = Page and Page:FindFirstChild("PageViewClipper")
+    local PageView = PageViewClipper and PageViewClipper:FindFirstChild("PageView")
+    local PageViewInnerFrame = PageView and PageView:FindFirstChild("PageViewInnerFrame")
+    local InnerPage = PageViewInnerFrame and PageViewInnerFrame:FindFirstChild("Page")
+    local MaxFrameRateFrame = InnerPage and InnerPage:FindFirstChild("Maximum Frame RateFrame")
+    local DropDownFrameButton = MaxFrameRateFrame and MaxFrameRateFrame:FindFirstChild("DropDownFrameButton")
+    local DropDownFrameTextLabel = DropDownFrameButton and DropDownFrameButton:FindFirstChild("DropDownFrameTextLabel")
+    if DropDownFrameTextLabel then
+        userFpsCap = tonumber(DropDownFrameTextLabel.Text:match("%d+")) or userFpsCap
+    end
+    return userFpsCap
+end
+
 UserInputService.WindowFocusReleased:Connect(function()
     RunService:Set3dRenderingEnabled(false)
     blackFrame.Visible = true
@@ -44,9 +50,7 @@ UserInputService.WindowFocusReleased:Connect(function()
 end)
 
 UserInputService.WindowFocused:Connect(function()
-	local MaxFrameRate = tonumber(DropDownFrameTextLabel.Text:match("%d+")) or 60
-
     RunService:Set3dRenderingEnabled(true)
     blackFrame.Visible = false
-    setfpscap(MaxFrameRate)
+    setfpscap(getMaxFrameRate())
 end)
